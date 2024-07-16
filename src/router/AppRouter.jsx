22832e735a73407db840f220872b9be9acb568c1
shounas99 <to.aguilar.rodriguez.cristina@gmail.com>
2024-07-16 13:43:22 -0600

    Add - Routes publics and privates

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,17 +2,34 @@ import { HeroesRoutes } from '../heroes'
 import { LoginPage } from "../auth"
 import { Navbar } from "../ui"
 import { Route, Routes } from "react-router"
+import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
     return (
         <>
-        <Navbar />
-        <div className='container'>
-            <Routes>
-                <Route path="/login" element={ <LoginPage/>} /> 
-                <Route path="/*" element={ <HeroesRoutes/>} /> 
-            </Routes>
-        </div>
+            <Navbar />
+            <div className='container'>
+                <Routes>
+                    {/* -----Explicit-----*/}
+                    <Route path="login/*" element={
+                        <PublicRoute>
+                            <Routes>
+                                <Route path='/*' element={ <LoginPage/> }/>
+                            </Routes>
+                            {/* <LoginPage/> */}
+                        </PublicRoute>
+                    }/>
+                    {/* -----By modules----- */}
+                    <Route path="/*" element ={
+                        <PrivateRoute >
+                            <HeroesRoutes/>
+                        </PrivateRoute>
+                    } />
+                    {/* <Route path='/login' element={ <LoginPage/> }/> */}
+                    {/* <Route path='/*' element={ <HeroesRoutes /> }/> */}
+                </Routes>
+            </div>
         </>
     )
 }
